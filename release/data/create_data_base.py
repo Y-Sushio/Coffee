eb9a29b65ea867acea04de92efc7f3ec04b10c42
Viktor <vp7780590@gmail.com>
2024-01-11 12:07:30 +0300

    Скомпилирован исполняемый файл

---
+++ release/data/create_data_base.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+with sqlite3.connect("coffee.sqlite") as data_base:
+    cursor = data_base.cursor()
+
+    dict_grade = {"Арабика": ("Вкусный", 350), "Робуста": ("Очень вкусный", 450),
+                  "Либерика": ("Неплохой кофе", 150), "Эксцельса": ("Хороший кофе", 275)}
+
+    count = 1
+    for grade in list(dict_grade.keys()):
+        for degree_roasting in ["Слабая", "Средняя", "Сильная"]:
+            for volume in [50, 100, 150, 200]:
+                for grind in [False, True]:
+                    cursor.execute("""INSERT INTO Coffee VALUES
+                    (?, ?, ?, ?, ?, ?, ?)""", (count, grade, degree_roasting, grind, dict_grade[grade][0],
+                                               dict_grade[grade][1] * (volume / 100) + 25 * (volume / 100) if grind else
+                                               dict_grade[grade][1] * (volume / 100), volume))
+
+                    count += 1
